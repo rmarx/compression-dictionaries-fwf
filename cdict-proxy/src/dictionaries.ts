2b6160e55674fa0fe2ac26443679f56329172e08
Radu Matei <radu@fermyon.com>
2025-02-27 23:19:49 +0100

    Update fetch calls to always use local service chaining to ensure the same node handles the request
    Signed-off-by: Radu Matei <radu@fermyon.com>

--- cdict-proxy/src/dictionaries.ts
+++ cdict-proxy/src/dictionaries.ts
@@ -1,14 +1,15 @@
 import { IRequestStrict } from "itty-router";
 
-export async function getDictionary( name:String, req:IRequestStrict ) {
+export async function getDictionary(name: String, req: IRequestStrict) {
     // name should already be the full name relative to assets/dictionaries/
-    const dictResponse = await fetch(`/static/dictionaries/${name}`);
+    // const dictResponse = await fetch(`https://self.alt/static/dictionaries/${name}`);
+    const dictResponse = await fetch(`https://assets.spin.internal/dictionaries/${name}`);
 
     console.log(`[dictionaries]: fetched ${name}`);
 
     let headers: Headers = new Headers();
 
-    if ( name === "common.dict" ) {
+    if (name === "common.dict") {
 
         // note: Content-Encoding HAS TO BE UNSET or chrome won't accept (even though it's technically a zstd-generated dictionary)
         headers.set("Cache-Control", `public, max-age=2592000`);
@@ -19,7 +20,7 @@ export async function getDictionary( name:String, req:IRequestStrict ) {
         // match="/pages/*", match-dest=("document"), id="common.dict"
     }
 
-    else if ( name === "big-specific.dict" ) {
+    else if (name === "big-specific.dict") {
 
         headers.set("Cache-Control", `public, max-age=2592000`);
         headers.set("Content-Type", `text/plain; charset=UTF-8`);
